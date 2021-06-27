b5cc53619956e56599cbad72a4c672f353a5690f
Ruudra Patel <ruudzrocks@gmail.com>
2021-06-27 11:55:24 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Javascript and ReactJS</h1>
+      <p>Basics of Javascript and React.</p>
+      <ol className="note-ol">
+        <li>Introduction :</li>
+        <br />
+        <ul>
+          <li>Who invented Javascript</li>
+          <li>Naming Conventions</li>
+          <li>Structure of Web Development</li>
+        </ul>
+        <br />
+        <li>Datatypes in Javascript:</li>
+        <br />
+        <ul>
+          <li>Types of Datatypes</li>
+          <li>Examples of Different Datatypes</li>
+        </ul>
+        <br />
+        <li>Functions</li>
+        <br />
+        <ul>
+          <li>Examples of Different Functions</li>
+        </ul>
+        <br />
+        <li>Basics of HTML and CSS</li>
+        <br />
+        <li>ReactJS</li>
+        <br />
+        <ul>
+          <li>Functions in ReactJS</li>
+          <li>Examples of Functions</li>
+        </ul>
+      </ol>
+      <p>Time: 7 days</p>
+    </div>
+  );
+}
+export default Info;
